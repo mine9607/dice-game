6e6b7e34eb496b4a8508ff455622b6f6b6debaa1
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-07-11 11:12:13 -0500

    Finished coding website with javascript functionality

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-document.querySelectorAll("img").classList.toggle("invisible");
+var randNum1 = Math.floor(Math.random() * 6) + 1;
+var randDiceImage1 = "./images/dice" + randNum1 + ".png";
+var dice1 = document.querySelectorAll("img")[0];
+dice1.setAttribute("src", randDiceImage1);
+
+var randNum2 = Math.floor(Math.random() * 6) + 1;
+var randDiceImage2 = "./images/dice" + randNum2 + ".png";
+var dice2 = document.querySelectorAll("img")[1];
+dice2.setAttribute("src", randDiceImage2);
+
+if (randNum1 == randNum2) {
+  document.querySelector("h1").textContent = "It's a Draw!";
+} else if (randNum1 > randNum2) {
+  document.querySelector("h1").textContent = "Player 1 Wins!";
+} else {
+  document.querySelector("h1").textContent = "Player 2 Wins!";
+}
